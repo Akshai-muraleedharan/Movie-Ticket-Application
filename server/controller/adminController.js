486ff2023d4626f54feb7c8db97921e4114cdfa6
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-08-20 12:13:24 +0100

    react vite and tailwind css succesfully installed

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,197 +1,203 @@
+import bcyrpt from "bcrypt";
+import { createToken } from "../utils/generateToken.js";
+import AdminModel from "../models/adminModel.js";
+import { cloudinaryInstance } from "../config/cloudneryConfig.js";
 
-import bcyrpt from 'bcrypt'
-import { createToken } from '../utils/generateToken.js'
-import AdminModel from '../models/adminModel.js'
-import { cloudinaryInstance } from '../config/cloudneryConfig.js'
+export const adminSignup = async (req, res) => {
+  try {
+    const { username, email, password, profilePic } = req.body;
 
-
-export const adminSignup = async (req,res) => {
-    try {
-       
-        const {username,email,password,profilePic} = req.body
-
-        if(!username || !email || !password ) {
-            return res.status(400).json({success:false,message:"All fields are required"})
-        }
-        const adminExist = await AdminModel.findOne({email})
-        
-        if(adminExist){
-            return res.status(400).json({success:false,message:"admin already exist"})
-            
-        }
-
-       const totalAdmin = await AdminModel.find();
-        const adminLength = totalAdmin.length + 1
-       
-        if(adminLength > 4){
-            return res.status(400).json({success:false,message:"maximum number of admins added"})
-        }
-        
-        
-        const salt = 10
-        const hashedPassword = bcyrpt.hashSync(password,salt)
-
-        const NewAdmin = new AdminModel({username,email,password:hashedPassword,profilePic}) 
-        await NewAdmin.save()
-
-        const token = createToken(email,"admin")
- 
-        res.cookie('token',token)
-        
-        res.status(200).json({success:true,message:"admin signup successfully"})
-    } catch (error) {
-        console.log(error)
-        res.status(error.status || 500).json({message:error || "internal server error"})
-       
-        
-    } 
-}
-
-
-
-export const adminLogin = async (req,res,next) => {
-    try {
-        const {email,password} = req.body
-
-        if( !email || !password ) {
-            return res.status(400).json({success:false,message:"All fields are required"})
-        }
-        const userExist = await AdminModel.findOne({email})
-        
-        if(!userExist){
-            return res.status(400).json({success:false,message:"admin doesn't exist"})
-            
-        }
-        
-
-        const passwordMatch = bcyrpt.compareSync(password,userExist.password)
-
-        if(!passwordMatch){
-            return res.status(400).json({success:false,message:"admin not Authenticaed" })
-            }
-        
-        const token = createToken(email,"admin")
- 
-        res.cookie('token',token)
-        
-        res.status(200).json({success:true,message:"owner login successfully"})
-    } catch (error) {
-        res.status(error.status || 500).json({message:error || "internal server error"})
-       
-        
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
     }
-}
+    const adminExist = await AdminModel.findOne({ email });
 
-
-export const adminUpdate = async (req,res) => {
-    const {username} =req.body;
-    const {verifiedAdmin} = req.admin;
-    let image;
-
-    const admin = await  AdminModel.findOneAndUpdate(verifiedAdmin)
-    
-    if(!req.file ){
-        image = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
-    }else{
-        image = req.file.path
-    }
-    const uploadResult = await cloudinaryInstance.uploader.upload(image,{folder:'movie ticket application/admin profile'})
-      .catch((error)=>{
-        console.log(error)
-    })
-
-
-    const updatedData = await AdminModel.findOneAndUpdate(admin,{
-        username,
-        profilePic:uploadResult.url
-    },{new:true})
-
-    res.json({success:true,message:"updated successessfully",data:updatedData})
-
-}
-
-
-
-export const adminProfile= async (req,res,next) => {
-    try {
-       const {verifiedAdmin} = req.admin;
-       
-       const adminProfileData = await AdminModel.findOne(verifiedAdmin).select('-password')
-  
-        
-       if(!adminProfileData){
-       return res.status(400).json({success:false,message:"no account"})
-       }
-    
-       res.status(200).json({success:true,message:adminProfileData})
-       
-       
-    } catch (error) {
-        console.log(error)
-        res.status(error.status || 500).json({message:error || "internal server error"})
-       
-        
-    }
-} 
-
-
-export const adminLogout= async (req,res,next) => {
-    try {
-           
-        res.clearCookie('token')
-
-        res.json({success:true,message:"admin logout"})
-       
-       
-    } catch (error) {
-        res.status(error.status || 500).json({message:error || "internal server error"})
-       
-        
-    }
-} 
-
-export const checkAdmin= async (req,res,next) => {
-    try {
-       
-      const verifiedAdmin = req.admin;
-   
-      if(!verifiedAdmin){
-      return  res.status(400).json({success:false,message:"admin not authenticated"})
-      }
-      res.json({success:true,message:"admin authenticatd"})
-     
-        
-    } catch (error) {
-        console.log(error)
-        res.status(error.status || 500).json({message:error || "internal server error"})
-       
-        
-    }
-} 
-
-
-export const adminDelete = async (req,res) => {
-
-    try {
-
-        const {id} = req.params
-   
-   
-        const accountExist = await AdminModel.findById(id)
-
-        if(!accountExist){
-            return res.status(400).json({success:false,message:"your account could not delete now"})
-        }else{
-            res.clearCookie('token')
-            await AdminModel.findByIdAndDelete(id)
-            res.json({success:true,message:"your account deleted successfully"})
-        }
-
-      
-
-    } catch (error) {
-        
-        res.status(error.status || 500).json({message:error || "internal server error"})
+    if (adminExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "admin already exist" });
     }
 
-}
+    const totalAdmin = await AdminModel.find();
+    const adminLength = totalAdmin.length + 1;
+
+    if (adminLength > 4) {
+      return res
+        .status(400)
+        .json({ success: false, message: "maximum number of admins added" });
+    }
+
+    const salt = 10;
+    const hashedPassword = bcyrpt.hashSync(password, salt);
+
+    const NewAdmin = new AdminModel({
+      username,
+      email,
+      password: hashedPassword,
+      profilePic,
+    });
+    await NewAdmin.save();
+
+    const token = createToken(email, "admin");
+
+    res.cookie("token", token);
+
+    res
+      .status(200)
+      .json({ success: true, message: "admin signup successfully" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(error.status || 500)
+      .json({ message: error || "internal server error" });
+  }
+};
+
+export const adminLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const userExist = await AdminModel.findOne({ email });
+
+    if (!userExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "admin doesn't exist" });
+    }
+
+    const passwordMatch = bcyrpt.compareSync(password, userExist.password);
+
+    if (!passwordMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "admin not Authenticaed" });
+    }
+
+    const token = createToken(email, "admin");
+
+    res.cookie("token", token);
+
+    res
+      .status(200)
+      .json({ success: true, message: "owner login successfully" });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json({ message: error || "internal server error" });
+  }
+};
+
+export const adminUpdate = async (req, res) => {
+  const { username } = req.body;
+  const { verifiedAdmin } = req.admin;
+  let image;
+
+  const admin = await AdminModel.findOneAndUpdate(verifiedAdmin);
+
+  if (!req.file) {
+    image =
+      "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
+  } else {
+    image = req.file.path;
+  }
+  const uploadResult = await cloudinaryInstance.uploader
+    .upload(image, { folder: "movie ticket application/admin profile" })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  const updatedData = await AdminModel.findOneAndUpdate(
+    admin,
+    {
+      username,
+      profilePic: uploadResult.url,
+    },
+    { new: true }
+  );
+
+  res.json({
+    success: true,
+    message: "updated successessfully",
+    data: updatedData,
+  });
+};
+
+export const adminProfile = async (req, res, next) => {
+  try {
+    const { verifiedAdmin } = req.admin;
+
+    const adminProfileData = await AdminModel.findOne(verifiedAdmin).select(
+      "-password"
+    );
+
+    if (!adminProfileData) {
+      return res.status(400).json({ success: false, message: "no account" });
+    }
+
+    res.status(200).json({ success: true, message: adminProfileData });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(error.status || 500)
+      .json({ message: error || "internal server error" });
+  }
+};
+
+export const adminLogout = async (req, res, next) => {
+  try {
+    res.clearCookie("token");
+
+    res.json({ success: true, message: "admin logout" });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json({ message: error || "internal server error" });
+  }
+};
+
+export const checkAdmin = async (req, res, next) => {
+  try {
+    const verifiedAdmin = req.admin;
+
+    if (!verifiedAdmin) {
+      return res
+        .status(400)
+        .json({ success: false, message: "admin not authenticated" });
+    }
+    res.json({ success: true, message: "admin authenticatd" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(error.status || 500)
+      .json({ message: error || "internal server error" });
+  }
+};
+
+export const adminDelete = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const accountExist = await AdminModel.findById(id);
+
+    if (!accountExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "your account could not delete now" });
+    } else {
+      res.clearCookie("token");
+      await AdminModel.findByIdAndDelete(id);
+      res.json({ success: true, message: "your account deleted successfully" });
+    }
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json({ message: error || "internal server error" });
+  }
+};
--- server/controller/movieController.js
+++ server/controller/movieController.js
@@ -1,103 +1,102 @@
-import { cloudinaryInstance } from '../config/cloudneryConfig.js'
-import NewMovieModel from '../models/newMovieModel.js'
+import { cloudinaryInstance } from "../config/cloudneryConfig.js";
+import NewMovieModel from "../models/newMovieModel.js";
 
-export const movieCreate = async (req,res,next) => {
-    try {
-
-       
-        
-        const {title,desc,rating,duration,genure,} =req.body
-
-        const movieExist =await NewMovieModel.findOne({title})
-
-        if(!req.file ){
-           return res.status(400).json({success:false,message:"please add movie image"})
-        }
-
-        const uploadResult = await cloudinaryInstance.uploader.upload(req.file.path,{folder:'movie ticket application/movies'})
-          .catch((error)=>{
-            console.log(error)
-        })
-
-        if(movieExist){
-          
-           return res.status(400).json({success:false,message:"movie already exist"})
-        }
-
-          const timeShedule =['12:00pm','03:00pm','06:00:pm','12:00pm']
-
-        const newMovie = new NewMovieModel({
-            title,
-            desc,
-            image:uploadResult.url,
-            rating,
-            duration,
-            genure,
-            showTime:timeShedule
-        })
-
-        await newMovie.save()
-        
-        res.status(200).json({success:true,message:" movie create successfully"})
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-} 
-
-export const movieList = async (req,res) => {
+export const movieCreate = async (req, res, next) => {
   try {
-    const allMovie = await NewMovieModel.find()
-  
-    const moviesLength = allMovie.length;
-   if(moviesLength <= 0){
-       return res.status(400).json({success:false,message:"no movies",movieLenth:moviesLength})
-   }
-   res.json({status:true,movies:allMovie,movieLenth:moviesLength})
+    const { title, desc, rating, duration, genure } = req.body;
 
+    const movieExist = await NewMovieModel.findOne({ title });
+
+    if (!req.file) {
+      return res
+        .status(400)
+        .json({ success: false, message: "please add movie image" });
+    }
+
+    const uploadResult = await cloudinaryInstance.uploader
+      .upload(req.file.path, { folder: "movie ticket application/movies" })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    if (movieExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "movie already exist" });
+    }
+
+    const timeShedule = ["12:00pm", "03:00pm", "06:00:pm", "12:00pm"];
+
+    const newMovie = new NewMovieModel({
+      title,
+      desc,
+      image: uploadResult.url,
+      rating,
+      duration,
+      genure,
+      showTime: timeShedule,
+    });
+
+    await newMovie.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: " movie create successfully" });
   } catch (error) {
     console.log(error);
-    
   }
-}
+};
 
-export const movieUpdate = async (req,res) => { 
-   try {
-   
-    
-    const {title,desc,image,rating,duration,genure} = req.body;
+export const movieList = async (req, res) => {
+  try {
+    const allMovie = await NewMovieModel.find();
 
-    const {id} = req.params
+    const moviesLength = allMovie.length;
+    if (moviesLength <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "no movies",
+        movieLenth: moviesLength,
+      });
+    }
+    res.json({ status: true, movies: allMovie, movieLenth: moviesLength });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const movieUpdate = async (req, res) => {
+  try {
+    const { title, desc, image, rating, duration, genure } = req.body;
 
+    const { id } = req.params;
 
-    const timeShedule =['12:00pm','03:00pm','07:00:pm','1:00pm']
+    const timeShedule = ["12:00pm", "03:00pm", "07:00:pm", "1:00pm"];
 
-    const updatedMovie = await NewMovieModel.findByIdAndUpdate(id,{
+    const updatedMovie = await NewMovieModel.findByIdAndUpdate(
+      id,
+      {
         title,
         desc,
         image,
-        rating,
+        rating,P
         duration,
         genure,
-        showTime:timeShedule
-    },{new:true})
+        showTime: timeShedule,
+      },
+      { new: true }
+    );
 
-    res.json({success:true,message:"movie updated", updatedMovie})
-   } catch (error) {
+    res.json({ success: true, message: "movie updated", updatedMovie });
+  } catch (error) {
     console.log(error);
-    
-   }
-}
+  }
+};
 
-export const movieDelete = async (req,res) => {
+export const movieDelete = async (req, res) => {
+  const { id } = req.params;
 
-    const {id} = req.params
+  await NewMovieModel.findByIdAndDelete(id);
 
-    
-
-    await NewMovieModel.findByIdAndDelete(id)
-
-    res.json({success:true,message:"deleted successfully"}) 
-}  
+  res.json({ success: true, message: "deleted successfully" });
+};
