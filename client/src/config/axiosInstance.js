caebb83ba71b3bd11dc6daab6bf6c03b236b9f95
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-08-30 14:37:32 +0100

    bacend update

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
     // baseURL:'http://localhost:4005/api/v1',
-    baseURL:'https://movie-ticket-application-backend-puce.vercel.app',
+    baseURL:'https://movie-ticket-application-backend-puce.vercel.app/api/v1',
     withCredentials:true
 });
