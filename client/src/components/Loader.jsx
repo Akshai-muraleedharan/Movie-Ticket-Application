af735228222da545e1150ad7ad771b102ba4e6e5
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-09-10 06:22:34 +0100

    git update

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,8 +4,8 @@ import './Loader.css'; // Assume you have some styles in Loader.css
 
 const Loader = () => {
   return (
-    <div class="spinner">
-    <div class="lds-roller">
+    <div className="spinner">
+    <div className="lds-roller">
         <div></div>
         <div></div>
         <div></div>
