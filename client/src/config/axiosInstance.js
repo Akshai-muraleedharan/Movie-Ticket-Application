e6ca1caeda17f0c5a67ec2d8562ba163880874f9
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-09-10 23:53:32 +0100

    deployment update

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 export const axiosInstance = axios.create({
- //  baseURL:'http://localhost:4005/api/v1',
-     baseURL:`${import.meta.env.VITE_BASE_URL}/api/v1`,
+//  baseURL:'http://localhost:4005/api/v1',
+    baseURL:`${import.meta.env.VITE_BASE_URL}/api/v1`,
     withCredentials:true
    
 });
